c188e04ba181a6dbb3c48b6477776b183d14d98e
Vadym Usov <onlinemovement@gmail.com>
2021-03-04 21:28:41 +0200

    homework #3

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,14 @@
-const app = require('../app')
+const app = require("../app");
 
-const PORT = process.env.PORT || 3000
+const db = require("../model/db");
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
-})
+const PORT = process.env.PORT || 3000;
+
+db.then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running. Use our API on port: ${PORT}`);
+  });
+}).catch((e) => {
+  console.log(`Server not running. Error message ${e.message}`);
+  process.exit(1);
+});
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,100 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const Contacts = require("../../model/contacts");
 const validate = require("./validation");
+const contactControllers = require("../../controllers/contacts");
 
-router.get("/", async (req, res, next) => {
-  try {
-    const contacts = await Contacts.getAll();
-    return res.json({
-      status: "success",
-      code: 200,
-      data: { contacts },
-    });
-  } catch (e) {
-    next(e);
-  }
-});
+router
+  .get("/", contactControllers.getAll)
+  .post("/", validate.createContact, contactControllers.create);
 
-router.get("/:id", async (req, res, next) => {
-  try {
-    console.log(req.params);
-    const contact = await Contacts.getById(req.params.id);
-
-    if (contact) {
-      return res.json({
-        status: "success",
-        code: 200,
-        data: { contact },
-      });
-    } else {
-      return res.status(404).json({
-        status: "error",
-        code: 404,
-        data: { message: "Not Found" },
-      });
-    }
-  } catch (e) {
-    next(e);
-  }
-});
-
-router.post("/", validate.createContact, async (req, res, next) => {
-  try {
-    const contact = await Contacts.create(req.body);
-    return res.status(201).json({
-      status: "success",
-      code: 201,
-      data: { contact },
-    });
-  } catch (e) {
-    next(e);
-  }
-});
-
-router.delete("/:id", async (req, res, next) => {
-  try {
-    const contact = await Contacts.remove(req.params.id);
-
-    if (contact) {
-      return res.json({
-        status: "success",
-        code: 200,
-        data: { message: "contact deleted" },
-      });
-    } else {
-      return res.status(404).json({
-        status: "error",
-        code: 404,
-        data: { message: "Not Found" },
-      });
-    }
-  } catch (e) {
-    next(e);
-  }
-});
-
-router.put("/:id", validate.updateContact, async (req, res, next) => {
-  try {
-    const contact = await Contacts.update(req.params.id, req.body);
-    console.log(contact);
-
-    if (contact) {
-      return res.json({
-        status: "success",
-        code: 200,
-        data: { contact },
-      });
-    } else {
-      return res.status(404).json({
-        status: "error",
-        code: 404,
-        data: { message: "Not Found" },
-      });
-    }
-  } catch (e) {
-    next(e);
-  }
-});
+router
+  .get("/:id", contactControllers.getById)
+  .put("/:id", validate.updateContact, contactControllers.update)
+  .delete("/:id", contactControllers.remove);
 
 module.exports = router;
