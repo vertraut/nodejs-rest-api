c48130f03c7fcd86328b1fef35d32fa1245857ad
Vadym Usov <onlinemovement@gmail.com>
2021-03-08 13:33:12 +0200

    adds an owner for contacts

--- model/schemas/contact.js
+++ model/schemas/contact.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const { Schema, model } = mongoose;
+const { Schema, model, SchemaTypes } = mongoose;
 
 const contactSchema = new Schema(
   {
@@ -17,6 +17,10 @@ const contactSchema = new Schema(
       type: String,
       required: [true, "User phone number required"],
       unique: true,
+    },
+    owner: {
+      type: SchemaTypes.ObjectId,
+      ref: "user",
     },
   },
   { versionKey: false, timestamps: true }
